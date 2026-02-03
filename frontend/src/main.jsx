e4f4e06e41897312f4cd355bbab3d1ce55eb9ae5
Aniket Shakya <143391154+aniketshakya17@users.noreply.github.com>
2026-02-03 23:36:35 +0530

    Set app title to 123 Fakturera

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,17 @@
-import React from 'react';
+import React from "react";
 import ReactDom from "react-dom/client";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-import Login from './login_page/Login';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./login_page/Login";
+import Dashboard from "./dashboard_page/Dashboard";
 import "./index.css";
-import Dashboard from './dashboard_page/Dashboard';
 import "./i18n";
 
-
 ReactDom.createRoot(document.getElementById("root")).render(
-    <BrowserRouter>
-      <Routes>
-         <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<Login></Login>}></Route>
-        <Route path="/dashboard" element = {<Dashboard></Dashboard>} ></Route>
-      </Routes>
-    </BrowserRouter>
-)
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  </BrowserRouter>
+);
